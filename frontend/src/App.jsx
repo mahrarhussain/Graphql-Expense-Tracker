8dbfb2e559f69812254eef29f972dbf927d87efe
mahrarhussain07 <mahrarhussain9@gmail.com>
2024-10-15 10:32:43 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Header from "./components/ui/Header";
 import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import NotFound from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import TransactionPage from "./pages/TransactionPage";
 
 function App() {
   const authUser = true;
@@ -10,10 +13,10 @@ function App() {
     <>
       {authUser && <Header />}
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        {/* <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        {/* <Route path="/transaction/:id" element={<TransactionPage />} /> */}
+        <Route path="/transaction/:id" element={<TransactionPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
